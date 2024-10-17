905bb87c4038a7de053e1e3383f803bbb541f134
Ярослав Родионов <iaroslav@MacBook-Pro--Iaroslav.local>
2024-10-17 18:10:36 +0300

    add test, fix code

--- src/main/java/hexlet/code/controller/TaskStatusController.java
+++ src/main/java/hexlet/code/controller/TaskStatusController.java
@@ -7,16 +7,24 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.RestController;
+
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/task_statuses")
 public class TaskStatusController {
 
-    private static final Logger logger = LoggerFactory.getLogger(TaskStatusController.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(TaskStatusController.class);
 
     @Autowired
     private TaskStatusService taskStatusService;
@@ -28,7 +36,7 @@ public class TaskStatusController {
      */
     @GetMapping
     public List<TaskStatus> getAllTaskStatuses() {
-        logger.info("Fetching all task statuses.");
+        LOGGER.info("Fetching all task statuses.");
         return taskStatusService.getAllTaskStatuses();
     }
 
@@ -40,7 +48,7 @@ public class TaskStatusController {
      */
     @GetMapping("/{id}")
     public TaskStatus getTaskStatusById(@PathVariable Long id) {
-        logger.info("Fetching task status with id: {}", id);
+        LOGGER.info("Fetching task status with id: {}", id);
         return taskStatusService.getTaskStatusById(id);
     }
 
@@ -52,9 +60,9 @@ public class TaskStatusController {
      */
     @PostMapping
     public ResponseEntity<TaskStatus> createTaskStatus(@Valid @RequestBody TaskStatus taskStatus) {
-        logger.info("Creating new task status: {}", taskStatus);
+        LOGGER.info("Creating new task status: {}", taskStatus);
         TaskStatus createdTaskStatus = taskStatusService.createTaskStatus(taskStatus);
-        logger.info("Task status created successfully: {}", createdTaskStatus);
+        LOGGER.info("Task status created successfully: {}", createdTaskStatus);
         return ResponseEntity.status(201).body(createdTaskStatus);
     }
 
@@ -67,9 +75,9 @@ public class TaskStatusController {
      */
     @PutMapping("/{id}")
     public TaskStatus updateTaskStatus(@PathVariable Long id, @Valid @RequestBody TaskStatus taskStatus) {
-        logger.info("Updating task status with id: {}, data: {}", id, taskStatus);
+        LOGGER.info("Updating task status with id: {}, data: {}", id, taskStatus);
         TaskStatus updatedTaskStatus = taskStatusService.updateTaskStatus(id, taskStatus);
-        logger.info("Task status updated successfully: {}", updatedTaskStatus);
+        LOGGER.info("Task status updated successfully: {}", updatedTaskStatus);
         return updatedTaskStatus;
     }
 
@@ -83,17 +91,17 @@ public class TaskStatusController {
     @PatchMapping("/{id}")
     public ResponseEntity<?> partialUpdateTaskStatus(@PathVariable Long id,
                                                      @RequestBody TaskStatusUpdateDto taskStatusUpdateDto) {
-        logger.info("Received request to partially update TaskStatus with id: {}", id);
-        logger.info("DTO Name: {}, DTO Slug: {}", taskStatusUpdateDto.getName(), taskStatusUpdateDto.getSlug());
+        LOGGER.info("Received request to partially update TaskStatus with id: {}", id);
+        LOGGER.info("DTO Name: {}, DTO Slug: {}", taskStatusUpdateDto.getName(), taskStatusUpdateDto.getSlug());
 
         try {
             TaskStatus updatedTaskStatus = taskStatusService.partialUpdateTaskStatus(id, taskStatusUpdateDto);
-            logger.info("TaskStatus updated successfully. New values: Name: {}, Slug: {}",
+            LOGGER.info("TaskStatus updated successfully. New values: Name: {}, Slug: {}",
                     updatedTaskStatus.getName(), updatedTaskStatus.getSlug());
 
             return ResponseEntity.ok(updatedTaskStatus);
         } catch (RuntimeException e) {
-            logger.error("Error during update: {}", e.getMessage());
+            LOGGER.error("Error during update: {}", e.getMessage());
             return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
@@ -106,9 +114,9 @@ public class TaskStatusController {
      */
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteTaskStatus(@PathVariable Long id) {
-        logger.info("Deleting task status with id: {}", id);
+        LOGGER.info("Deleting task status with id: {}", id);
         taskStatusService.deleteTaskStatus(id);
-        logger.info("Task status with id: {} deleted successfully.", id);
+        LOGGER.info("Task status with id: {} deleted successfully.", id);
         return ResponseEntity.noContent().build();
     }
 }
